caf220c221561480e0561dfd49e352c80a42c9ac
csharpd <chloedonegan@gmail.com>
2014-08-18 13:31:37 +0100

    Display message when no post and title when there is a post

--- spec/features/post_features_spec.rb
+++ spec/features/post_features_spec.rb
@@ -1,1 +1,22 @@
 require 'rails_helper'
+
+describe 'posts' do
+  context 'before anyone posts' do
+    it 'displays no posts' do
+      visit '/posts'
+      expect(page).to have_content 'There are no posts'
+    end
+  end
+
+   context 'with posts' do
+    before do
+      Post.create(title: 'Hello World')
+    end
+
+    it 'displays the posts title' do
+      visit '/posts'
+      expect(page).to have_content 'Hello World'
+    end
+   end
+end
+
